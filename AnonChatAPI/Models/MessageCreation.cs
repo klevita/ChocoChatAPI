635dfbea20316eecc653f70656ec5b7763076ad0
Alpha-Dolphi <djedai@list.ru>
2022-05-29 01:23:21 +0300

    inproved user registration, improved description, Tags model changed to an array, bug fixes

---
+++ AnonChatAPI/Models/MessageCreation.cs
@@ -0,0 +1,22 @@
+﻿//using MongoDB.Bson;
+//using MongoDB.Bson.Serialization.Attributes;
+
+//namespace AnonChatAPI.Models
+//{
+//	public class MessageCreation
+//	{
+//		[BsonElement("MessageF")]
+//		[BsonRepresentation(BsonType.ObjectId)]
+//		public string MessageF { get; set; } = null!;
+
+//		[BsonElement("Content")]
+//		public string Content { get; set; } = null!;
+
+//		[BsonElement("Date")]
+//		public string Date { get; set; } = null!;
+
+//		[BsonElement("MessageU")]
+//		[BsonRepresentation(BsonType.ObjectId)]
+//		public string MessageU { get; set; } = null!;
+//	}
+//}
---
+++ AnonChatAPI/Models/UserRegistration.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnonChatAPI.Models
+{
+    public class UserRegistration
+    {
+        [BsonElement("NickName")]
+        public string NickName { get; set; } = null!;
+        [Required(ErrorMessage = "Email is required")]
+
+        [BsonElement("Email")]
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; } = null!;      
+    }
+}
